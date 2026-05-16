ce9b378170ca03c76514d4e7764f8b528a553915
Pedro Fernandes Steimbruch <pedrofsteimbruch@gmail.com>
2026-05-16 13:48:33 -0300

    Refactor enrollments_controller

--- app/controllers/customer/organizations/campaigns/enrollments_controller.rb
+++ app/controllers/customer/organizations/campaigns/enrollments_controller.rb
@@ -1,23 +1,12 @@
 # frozen_string_literal: true
 
 class Customer::Organizations::Campaigns::EnrollmentsController < Customer::BaseController
+  before_action :ensure_customer
+
   def create
     campaign = @organization.campaigns.active_now.find_by!(slug: params[:slug])
 
-    customer = @current_customer || Customer.identify_for(
-      phone: enrollment_params[:phone],
-      cookie_jar: cookies
-    )
-
-    unless customer
-      redirect_to(
-        customer_organization_campaign_path(@organization.slug, campaign.slug),
-        inertia: { errors: { phone: "Número de WhatsApp inválido" } }
-      )
-      return
-    end
-
-    campaign.enroll!(customer: customer)
+    campaign.enroll!(customer: @customer)
 
     redirect_to(
       customer_organization_campaign_path(@organization.slug, campaign.slug),
@@ -26,6 +15,20 @@ class Customer::Organizations::Campaigns::EnrollmentsController < Customer::Base
   end
 
   private
+
+  def ensure_customer
+    @customer = @current_customer || Customer.identify_for(
+      phone: enrollment_params[:phone],
+      cookie_jar: cookies
+    )
+
+    return if @customer
+
+    redirect_to(
+      customer_organization_campaign_path(@organization.slug, campaign.slug),
+      inertia: { errors: { phone: "Número de WhatsApp inválido" } }
+    )
+  end
 
   def enrollment_params
     params.fetch(:enrollment, {}).permit(:phone)
